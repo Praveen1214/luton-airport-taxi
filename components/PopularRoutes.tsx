ab641de5d0e44f6b89707323ee93abdd826161b7
Dileepa Praveen <118685287+Praveen1214@users.noreply.github.com>
2025-05-25 11:47:46 +0530

    up

--- components/PopularRoutes.tsx
+++ components/PopularRoutes.tsx
@@ -4,7 +4,7 @@ export const routes = [
     { from: "Luton", to: "St Albans", duration: "1h 45m", distance: "66mi", slug: "st-albans" },
     { from: "Luton", to: "Stevenage", duration: "1h 45m", distance: "66mi", slug: "stevenage" },
     { from: "Luton", to: "London", duration: "1h 45m", distance: "66mi", slug: "london" },
-    { from: "Luton", to: "Aylesbury", duration: "1h 45m", distance: "66mi", slug: "aylesbury" },
+    { from: "Luton", to: "Aylesbury", duration: "1h 45m", distance: "66mi", slug: "ayelscbury" },
     { from: "Luton", to: "Hitchin", duration: "1h 45m", distance: "66mi", slug: "hitchin" },
     { from: "Luton", to: "Bedford", duration: "1h 45m", distance: "66mi", slug: "bedford" },
   ];
